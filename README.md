ed69fb960ac97d08adaf6b02f2b88d57ac723a84
mhw32 <me@mikewuis.me>
2022-12-01 10:09:21 -0800

    rename folders

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Alchemy Utilities
+# Block API Utilities
 
 Toolkit to make large batches of async queries to the Alchemy API to download a historical dataset of blocks and transactions. See [Alchemy's website](https://docs.alchemy.com/reference/api-overview) for more information on their API.
 
--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = open("README.md", "r").read()
 
 setup(
-    name="alchemy_utils",
+    name="block_api_utils",
     version="0.1.0",
     author="web3analytic",
     author_email="mike@paretolabs.xyz",
     packages=find_packages(),
     scripts=[],
-    description="Toolkit for parallel Alchemy API requests to fetch transactions.",
+    description="Toolkit for parallel API requests to fetch transactions.",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/web3analytic-xyz/alchemy-utils",
+    url="https://github.com/web3analytic-xyz/block-api-utils",
     install_requires=[
         "jsonlines==3.1.0",
         "numpy==1.21.4",
